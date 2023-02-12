f4d0de371c57c66faa25a7d6d6690dc72235f59f
Mossab weda <mossabweda@gmail.com>
2023-02-12 22:56:27 +0100

    Uploaf file

---
+++ Upload_File/Service/IFileUpload.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorInputFile;
+
+namespace Upload_File.Service
+{
+    public interface IFileUpload
+    {
+        Task UploadAsync(IFileListEntry file);
+    }
+}
